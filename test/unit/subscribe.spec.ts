e5754c06e125a11dafe662204232cd432f00c752
jkeegan2 <93603340+jkeegan2@users.noreply.github.com>
2022-07-06 09:47:28 -0400

    test: add tests for 100% code coverage (#36)
    * feat(store): add type completion for subscribing to store
    
    * test(tests): add more comprehensive tests of store functionality
    
    * docs(readme): add usage documentation
    
    * docs(readme): update and neaten usage section
    
    * fix(store): properly type the full state and handle skipUndo actions properly
    
    * test(test): split files and add more comprehensive tests
    
    * test: add missing tests for 100% coverage

--- test/unit/subscribe.spec.ts
+++ test/unit/subscribe.spec.ts
@@ -3,7 +3,7 @@ import * as assert from 'uvu/assert';
 import { createStore } from '../../src';
 import { delay, LOGIC } from './common';
 
-test('Subscribe to Present State', async () => {
+test('Subscribe to Present State (passing observer)', async () => {
   const STORE = createStore(LOGIC);
   let curCount = 0;
   STORE.subscribe((state) => {
@@ -14,6 +14,12 @@ test('Subscribe to Present State', async () => {
   STORE.dispatch({ name: 'INCREMENT_ASYNC' });
   await delay(60);
   assert.equal(curCount, 2);
+});
+
+test('Subscribe to Present State (no observer)', async () => {
+  const STORE = createStore(LOGIC);
+  const subscription = STORE.subscribe();
+  subscription.unsubscribe();
 });
 
 test.run();
